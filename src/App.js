69096b14be5f7048f4c8cdd8ad13a76a6daee877
Martin Hyberg <mhyberg@kth.se>
2019-03-04 21:45:15 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase/app'
+import 'firebase/database';
+
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super();
+
+        this.db = firebase.database().ref().child('Notes');
+        this.addNote = this.addNote.bind(this);
+        this.removeNote = this.removeNote.bind(this);
+
+        this.state={
+            notes:[]
+        }
+    }
+
+    componentDidMount(){
+        let prevNotes = this.state.notes;
+        this.db.on('child_added', snap => {
+            prevNotes.push({
+                id:snap.key,
+                text: snap.val().text});
+
+            this.setState({
+                notes:prevNotes
+            })
+        })
+
+        this.db.on('child_removed', snap => {
+            for (var i = 0; i < prevNotes.length; i++) {
+                if (prevNotes[i].id === snap.key) {
+                    prevNotes.splice(i, 1);
+                }
+            }
+
+            this.setState({
+                notes:prevNotes
+            })
+        })
+    }
+
+    addNote(e){
+        e.preventDefault();
+        if (e.target[0].value !== "") {
+            this.db.push().set({
+                'text': e.target[0].value
+            })
+        }
+    }
+
+    removeNote(id){
+        this.db.child(id).remove();
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.notes.map((item)=>{
+            return(
+                <h1 key={item.id} className="note" onClick={()=>{this.removeNote(item.id)}}>{item.text}</h1>
+            )
+        })}
+        <form onSubmit={this.addNote}>
+            <input type="text"/>
+            <button text="Add" type="submit">Add</button>
+
+        </form>
       </div>
     );
   }
