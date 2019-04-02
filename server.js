cb231c3fef33204efc57c1a7ac66f907acd484fa
Martin Hyberg <mhyberg@kth.se>
2019-04-02 19:44:09 +0200

    Deploy a'hoy

--- server.js
+++ server.js
@@ -4,49 +4,54 @@ const express = require('express')
 const fetch = require("node-fetch")
 const path = require('path');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
 dotenv.config(); // seys up the env variables
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/selectDestinationTime', (req, res) => {
-    console.log(req.query);
-    let urlParams = "";
-    for (let key in req.query) {
-        if (req.query.hasOwnProperty(key)) {
-            if(req.query[key] !== undefined)
-                urlParams += "&" + key + "=" + req.query[key]
-        }
-    }
-    fetch("https://api.sl.se/api2/TravelplannerV3_1/trip.json?" + urlParams)
-        .then(res => res.json())
-        .then(json => {
-            res.send(json)
-    });
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
 })
 
-app.get('/searchStation', (req, res) => {
-    console.log(req.query);
-    let requestURL = "https://api.sl.se/api2/typeahead.json?key=" + process.env.REACT_APP_PlatsuppslagAPI + "&searchstring=" + req.query.searchWord;
-    fetch(requestURL)
-        .then(res => res.json())
-        .then(json => {
-            res.send(json)
-    });
+//start server
+app.listen(port, (req, res) => {
+  console.log( `server listening on port: ${port}`);
 })
 
-
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+// app.get('/selectDestinationTime', (req, res) => {
+//     console.log(req.query);
+//     let urlParams = "";
+//     for (let key in req.query) {
+//         if (req.query.hasOwnProperty(key)) {
+//             if(req.query[key] !== undefined)
+//                 urlParams += "&" + key + "=" + req.query[key]
+//         }
+//     }
+//     fetch("https://api.sl.se/api2/TravelplannerV3_1/trip.json?" + urlParams)
+//         .then(res => res.json())
+//         .then(json => {
+//             res.send(json)
+//     });
+// })
+//
+// app.get('/searchStation', (req, res) => {
+//     console.log(req.query);
+//     let requestURL = "https://api.sl.se/api2/typeahead.json?key=" + process.env.REACT_APP_PlatsuppslagAPI + "&searchstring=" + req.query.searchWord;
+//     fetch(requestURL)
+//         .then(res => res.json())
+//         .then(json => {
+//             res.send(json)
+//     });
+// })
