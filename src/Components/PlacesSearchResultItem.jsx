5f25c8f8c1cd8e2f001e691416b88877443da1ae
Samuel <shert@kth.se>
2019-04-02 15:56:44 +0200

    Duplicate location fix

--- src/Components/PlacesSearchResultItem.jsx
+++ src/Components/PlacesSearchResultItem.jsx
@@ -1,17 +1,27 @@
 import React, {Component} from "react";
 import { connect } from 'react-redux'
 import { addPlace } from '../Actions/index.js'
+import _ from "lodash"
 
 class PlacesSearchResultItem extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            inputAlias: false,
+            messageCode: 0, //0: nominal, 1: enter alais, 2: place already exists
         }
     }
 
+    resetErrorMessage = _.debounce(() => {
+        this.setState({
+            messageCode:0,
+        });
+    }, 3000);
+
     addPlace = event => {
+        this.setState({
+            messageCode: 0,
+        });
         event.preventDefault();
         this.setState({inputAlias:false});
         let alias = event.target[0].value;
@@ -27,7 +37,18 @@ class PlacesSearchResultItem extends Component {
         this.props.addPlace(placeDBFormat, this.props.user.userID);
     }
 
-
+    evaluateAddPlace = () => {
+        let code = 1; //Ask for alias if everything is fine
+        for (var i = 0; i < this.props.places.length; i++) {
+            if(this.props.place.SiteId == this.props.places[i].location.id){
+                code = 2; //Place already exists
+                this.resetErrorMessage();
+            }
+        }
+        this.setState({
+            messageCode: code,
+        });
+    }
 
     render() {
         return (
@@ -35,14 +56,20 @@ class PlacesSearchResultItem extends Component {
                 <div className="places-search-result-item-name">
                     {this.props.place.Name}
                 </div>
-                <div className="places-search-result-item-button button" onClick={()=>{this.setState({inputAlias: true})}}>
+                <div className="places-search-result-item-button button" onClick={this.evaluateAddPlace}>
                     Add
                 </div>
                 <form className="places-search-result-item-alias" onSubmit={this.addPlace}>
-                    {this.state.inputAlias ?
+                    {this.state.messageCode === 1 ?
                         <input className="places-search-result-item-alias-field" type="text" placeholder="Alias"/>
-                        :""}
-                    </form>
+                        : ""
+                    }
+                    {this.state.messageCode === 2 ?
+                        <div className="place-error-message">
+                            You already have this station in another alias dumbo!
+                        </div> : ""
+                    }
+                </form>
                 </div>
         );
     }
