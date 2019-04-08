c5ef8b09838be01b6e3be6c2ef2fbdbbe19edc7a
Martin Hyberg <mhyberg@kth.se>
2019-04-08 14:00:02 +0200

    add func to deployed version

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 dotenv.config(); // seys up the env variables
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
+//Vet inte vrf man behöver denna, kan nog tas bort
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
@@ -23,11 +24,27 @@ if(process.env.NODE_ENV === 'production') {
 
   app.get('/searchStation', (req, res) => {
       console.log(req.query);
-      let requestURL = "https://api.sl.se/api2/typeahead.json?key=" + process.env.REACT_APP_PlatsuppslagAPI + "&searchstring=bergs";
+      let requestURL = "https://api.sl.se/api2/typeahead.json?key=" + process.env.REACT_APP_PlatsuppslagAPI + "&searchstring=" + req.query.searchWord;
       fetch(requestURL)
           .then(res => res.json())
           .then(json => {
               res.send(json)
+      });
+  })
+
+  app.get('/selectDestinationTime', (req, res) => {
+      console.log(req.query);
+      let urlParams = "";
+      for (let key in req.query) {
+          if (req.query.hasOwnProperty(key)) {
+              if(req.query[key] !== undefined)
+              urlParams += "&" + key + "=" + req.query[key]
+          }
+      }
+      fetch("https://api.sl.se/api2/TravelplannerV3_1/trip.json?key=" + process.env.REACT_APP_ReseplanerareAPI + "&" + urlParams)
+      .then(res => res.json())
+      .then(json => {
+          res.send(json)
       });
   })
 
@@ -39,38 +56,39 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 //build mode
-app.get('*', (req, res) => {
-    console.log("Bajs från SERVER: build mode");
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+app.get('/searchStation', (req, res) => {
+    console.log(req.query);
+    let requestURL = "https://api.sl.se/api2/typeahead.json?key=" + process.env.REACT_APP_PlatsuppslagAPI + "&searchstring=" + req.query.searchWord;
+    fetch(requestURL)
+    .then(res => res.json())
+    .then(json => {
+        res.send(json)
+    });
 })
+
+app.get('/selectDestinationTime', (req, res) => {
+    console.log(req.query);
+    let urlParams = "";
+    for (let key in req.query) {
+        if (req.query.hasOwnProperty(key)) {
+            if(req.query[key] !== undefined)
+            urlParams += "&" + key + "=" + req.query[key]
+        }
+    }
+    fetch("https://api.sl.se/api2/TravelplannerV3_1/trip.json?key=" + process.env.REACT_APP_ReseplanerareAPI + "&" + urlParams)
+    .then(res => res.json())
+    .then(json => {
+        res.send(json)
+    });
+})
+
+app.get('*', (req, res) => {
+    console.log(req);
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 
 //start server
 app.listen(port, (req, res) => {
   console.log( `server listening on port: ${port}`);
 })
-
-// app.get('/selectDestinationTime', (req, res) => {
-//     console.log(req.query);
-//     let urlParams = "";
-//     for (let key in req.query) {
-//         if (req.query.hasOwnProperty(key)) {
-//             if(req.query[key] !== undefined)
-//                 urlParams += "&" + key + "=" + req.query[key]
-//         }
-//     }
-//     fetch("https://api.sl.se/api2/TravelplannerV3_1/trip.json?" + urlParams)
-//         .then(res => res.json())
-//         .then(json => {
-//             res.send(json)
-//     });
-// })
-//
-// app.get('/searchStation', (req, res) => {
-//     console.log(req.query);
-//     let requestURL = "https://api.sl.se/api2/typeahead.json?key=" + process.env.REACT_APP_PlatsuppslagAPI + "&searchstring=" + req.query.searchWord;
-//     fetch(requestURL)
-//         .then(res => res.json())
-//         .then(json => {
-//             res.send(json)
-//     });
-// })
