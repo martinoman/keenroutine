943c2e1749e36d52d3d011cfe8cb681958e2a617
Martin Hyberg <mhyberg@kth.se>
2019-04-15 17:22:11 +0200

    searching for errors

--- server.js
+++ server.js
@@ -61,7 +61,8 @@ if(process.env.NODE_ENV === 'production') {
 
   app.get('*', (req, res) => {
       console.log("Från catchall prodmode");
-    res.sendFile(path.join(__dirname = 'client/build/index.html'));
+      console.log(path.join(__dirname = 'client/build/index.html'));
+      res.sendFile(path.join(__dirname = 'client/build/index.html'));
   })
 
 }else {
